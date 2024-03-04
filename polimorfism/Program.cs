bbb063196eb956168f73fa7000589b5fc02b1f44
Roberto <Roberto@DESKTOP-1818MJE>
2024-03-04 16:48:12 -0600

    Agregar archivos de proyecto.

---
+++ polimorfism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IVehicle
+{
+    void start();
+}
+
+public class Car : IVehicle
+{
+    public void start()
+    {
+        Console.WriteLine("The Car is starting...");
+    }
+}
+
+public class Motocycle : IVehicle
+{
+    public void start()
+    {
+        Console.WriteLine("The motocycle is starting...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IVehicle miAuto = new Car();
+        IVehicle miMoto = new Motocycle();
+
+        miAuto.start();
+        miMoto.start();
+    }
+}
